662a25908bcc8cf30fbbe3694cb91bdc1f8c8750
Beltrán Rengifo <b.rengifo@docline.es>
2021-03-11 22:17:52 +0100

    fix: global sass utils imports using style resources

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   publicRuntimeConfig: {
     baseUrl: process.env.BASE_URL,
   },
-  // Global page headers: https://go.nuxtjs.dev/config-head
+
   head: {
     title: 'Fakestore | A fake store made with Nuxt',
     htmlAttrs: {
@@ -27,31 +27,24 @@ export default {
     name: 'page-transition',
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['./assets/scss/main.scss'],
 
   styleResources: {
     scss: [
-      '@/assets/scss/_config.helpers.scss',
-      '@/assets/scss/_config.colors.scss',
-      '@/assets/scss/_config.fonts.scss',
-      '@/assets/scss/_config.responsive.scss',
-      '@/assets/scss/_utils.animations-transitions.scss',
+      '@/assets/scss/config/_config.helpers.scss',
+      '@/assets/scss/config/_config.colors.scss',
+      '@/assets/scss/config/_config.fonts.scss',
+      '@/assets/scss/config/_config.layout.scss',
+      '@/assets/scss/config/_config.responsive.scss',
     ],
   },
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+    '@nuxtjs/style-resources',
     [
       'nuxt-compress',
       {
@@ -65,16 +58,10 @@ export default {
     ],
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-  ],
+  modules: ['@nuxtjs/axios'],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     babel: {
       plugins: ['@babel/plugin-proposal-optional-chaining'],
