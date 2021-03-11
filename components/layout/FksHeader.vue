a911f89e04f335b7c584412e41b467c5f9a53dd3
Beltrán Rengifo <b.rengifo@docline.es>
2021-03-11 22:18:47 +0100

    feat: header component is wip

---
+++ components/layout/FksHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="header__inner"></div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'FksHeader',
+})
+</script>
+
+<style lang="scss" scoped>
+.header {
+  &__inner {
+    background-color: get-var(color-primary);
+    height: get-var(header-height);
+  }
+}
+</style>
