d3c2ba38034626966540d1e6e2711b966f87f1f2
Beltrán Rengifo <b.rengifo@docline.es>
2021-03-12 07:44:57 +0100

    fix: component auto imports

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,13 +9,8 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import FksHeader from '@/components/layout/FksHeader.vue'
 
 export default Vue.extend({
   name: 'DefaultLayout',
-
-  components: {
-    FksHeader,
-  },
 })
 </script>
