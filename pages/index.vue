abfb7d82b99b45d3cebd7067815c05208a5bb0f8
Beltrán Rengifo <b.rengifo@docline.es>
2021-03-13 15:38:19 +0100

    feat: vuex and async data integration for filtering

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <section class="home">
+    <fks-product-search />
     <fks-product-grid :products="products" />
   </section>
 </template>
@@ -7,13 +8,35 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Product } from '@/types/types'
+import { mapGetters } from 'vuex'
 
 export default Vue.extend({
   name: 'Home',
 
   async asyncData({ store }): Promise<Object> {
-    const products: Array<Product> = await store.dispatch('shop/getAllProducts')
+    await store.dispatch('shop/fetchProducts')
+
+    const products: Array<Product> = store.getters['shop/getProducts']
     return { products }
+  },
+
+  data() {
+    return {
+      products: [],
+    }
+  },
+
+  computed: {
+    ...mapGetters({
+      query: 'shop/getQuery',
+      filteredProducts: 'shop/getProducts',
+    }),
+  },
+
+  watch: {
+    query() {
+      this.products = this.filteredProducts
+    },
   },
 })
 </script>
