ffb7729d34b17e510fb6b1d6889e849615e5f05b
Beltrán Rengifo <b.rengifo@docline.es>
2021-03-13 15:38:46 +0100

    feat: search box and product search compoents

---
+++ components/common/FksSearchBox.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="search-box">
+    <span v-if="prependIcon && getIcon" class="search-box__prepend-icon">
+      <img :src="getIcon" alt="Search icon" width="16" height="16" />
+    </span>
+    <input
+      v-model="value"
+      type="text"
+      class="search-box__input"
+      :placeholder="placeholder"
+      @input="handleChange"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Nullable } from '@/types/types'
+
+export default Vue.extend({
+  name: 'FksSearchBox',
+
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Search',
+    },
+    prependIcon: {
+      type: String,
+      default: '',
+    },
+  },
+
+  data() {
+    return {
+      value: '' as String,
+    }
+  },
+
+  computed: {
+    getIcon(): Nullable<object> {
+      try {
+        return require(`@/assets/images/${this.prependIcon}.svg`)
+      } catch (e) {
+        return null
+      }
+    },
+  },
+
+  methods: {
+    handleChange() {
+      this.$emit('input', this.value)
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.search-box {
+  position: relative;
+
+  &__input {
+    display: block;
+    width: 100%;
+    border-radius: rem(10);
+    background-color: get-var(color-lighter-grey);
+    padding-left: rem(48);
+  }
+
+  &__prepend-icon {
+    position: absolute;
+    left: rem(24);
+    display: flex;
+    align-items: center;
+    height: 100%;
+    width: rem(16);
+  }
+}
+</style>
--- locale/en.ts
+++ locale/en.ts
@@ -9,8 +9,9 @@ export default {
     remove: 'Remove from cart',
     productAlreadyInCart: 'This product is already in your cart',
     productAddedToCart: 'Product added to your cart!',
-    productRemovedFromCart: 'Product removed from your cart!',
+    productRemovedFromCart: 'Product removed from your cart',
     emptyCartTitle: 'Your cart is empty',
     emptyCartContent: 'Go back to our shop to start adding products!',
+    searchProducts: 'Search product',
   },
 }
